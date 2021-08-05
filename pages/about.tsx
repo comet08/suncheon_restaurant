b40e4ec9190e3b9d9e7b8c036311a680ba283e65
comet <sperocometa@naver.com>
2021-08-05 19:34:55 +0900

    Create About Page

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+const About = ()=>{
+    return<div className="center">
+        💗🧡💛💚💙💜🤎🖤! 순천 맛집 리스트 ! 🖤🤎💜💙💚💛🧡💗 <br />
+        데이터 출처 : https://www.data.go.kr/data/3076984/fileData.do
+    </div>
+    
+}
+
+
+export default About;
