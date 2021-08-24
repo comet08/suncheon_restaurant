5f3074943fbe8e16c74bd0230bfc2ab3c56c258c
comet <sperocometa@naver.com>
2021-08-24 16:14:53 +0900

    Create Action for Save selected list : cache for history

--- redux/action/index.ts
+++ redux/action/index.ts
@@ -1,18 +1,26 @@
 const GET_LIST = 'GET_LIST';
+const SAVE_SELECTED = 'SAVE_SELECTED';
 
 import { RestaurantObj } from '../../pages/api/InterfaceAndType';
 import { request } from '../../pages/api/suncheon';
 
+export const getList = () => {
+  return (dispatch: any) => {
+    request().then((res) => {
+      dispatch({
+        type: GET_LIST,
+        rlist: res,
+      });
+    });
+  };
+};
 
-export const getList = ()=>{ 
-    return((dispatch : any)=>{
-        request()
-        .then((res)=>{
-            dispatch({
-                type : GET_LIST,
-                rlist : res
-            });
-         });
-        
-    })
-}
+export const saveSelected = (dong: string, values: Array<RestaurantObj>) => {
+  return (dispatch: any) => {
+    dispatch({
+      type: SAVE_SELECTED,
+      dong,
+      values,
+    });
+  };
+};
