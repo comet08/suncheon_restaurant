0501b142b738dee250916344bedddcb353550a51
comet <sperocometa@naver.com>
2021-09-10 20:31:03 +0900

    Add kakao api

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,15 @@ import '../public/firebase/firebase';
 
 export default function Index() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>순천 맛집✔</title>
         <meta name="description" content="전라남도 순천시 맛집 리스트" />
         <link rel="icon" href="/favicon.ico" />
+        <script src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_KEY}&libraries=services&autoload=false`} async />
       </Head>
       <Home />
-      <footer className={styles.footer}>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
+      <footer>
         <div>&copy {new Date().getFullYear()} suncheon restaurant</div>
       </footer>
     </div>
